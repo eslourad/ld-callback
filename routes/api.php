4b73c0c932c49f89fcf7a02ad487fd292aca869e
eslourad <louards@emberspec.com>
2022-05-05 15:46:34 +0800

    ping, transaction and account callbacks added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Carbon\Carbon;
+use App\Models\Transaction;
+use App\Models\Account;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,48 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/ping', function (Request $request) {
+    return [
+        "nam" => "Lahat Dito Test Callback API",
+        "env" => "stage",
+        "ver" => "1.0.1",
+        "srv" => Carbon::now()->toDateTimeString()
+    ];
+});
+
+Route::post('/transaction', function (Request $request) {
+
+    $transaction = new Transaction;
+    $transaction->current_time = $request->input('current_time');
+    $transaction->status = $request->input('status');
+    $transaction->order_id = $request->input('order_id');
+    $transaction->trx_id = $request->input('trx_id');
+    $transaction->message = $request->input('message');
+    $transaction->save();
+
+
+    return [
+        "message" => "OK"
+    ];
+});
+
+Route::post('/account', function (Request $request) {
+
+    $email = $request->input('email') ? $request->input('email') : 'no email';
+    $phone = $request->input('phone') ? $request->input('phone') : 'no phone';
+
+    $transaction = new Account;
+    $transaction->internal_user_id = $request->input('internal_user_id');
+    $transaction->token = $request->input('token');
+    $transaction->token_id = $request->input('token_id');
+    $transaction->email = $email;
+    $transaction->phone = $phone;
+    $transaction->save();
+
+
+    return [
+        "status" => "OK"
+    ];
 });
